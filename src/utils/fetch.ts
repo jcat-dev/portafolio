f8ff5250bcdeaa8ef3ecbe21e7012d9ec331ab49
unknown <jec.dev.22@gmail.com>
2023-10-20 23:54:50 -0300

    Refactorizar fetch.ts

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,26 +1,10 @@
 type Method = 'GET' | 'POST' | 'PUT' | 'DELETE'
 
-export const getFetch = (api: string) => {
-  return setFetch(api, 'GET')
-}
-
-export const postFetch = (api: string, body: object) => {
-  return setFetch(api, 'POST', body)
-}
-
-export const putFetch = (api: string, body: object) => {
-  return setFetch(api, 'PUT', body)
-}
-
-export const deleteFetch = (api:string) => {
-  return setFetch(api, 'DELETE')
-} 
-
-const setFetch = async (api: string, method: Method, body?: object) => {
+export const setFetch = (api: string, method: Method, body?: object) => {
   const config: RequestInit = {
     method,
     headers: {
-      'Content-Type': 'Application/json'
+      'Content-Type': 'application/json'
     }
   } 
 
