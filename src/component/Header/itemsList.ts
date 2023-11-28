f471f1577c3266ea25ce8475800747077880e92c
unknown <jec.dev.22@gmail.com>
2023-11-28 17:47:56 -0300

    Mover y Renombrar itemsList.ts a navItems.ts en pages/home

--- src/component/Header/itemsList.ts
+++
@@ -1,6 +0,0 @@
-export const itemsList = [
-  { value: 'Inicio', href: '#inicio' },
-  { value: 'Habilidades', href: '#habilidades' },
-  { value: 'Proyectos', href: '#proyectos' },
-  { value: 'Contacto', href: '#contacto' }
-]
---
+++ src/pages/home/navItems.ts
@@ -0,0 +1,8 @@
+import { NavItems } from '../../Types/NavItems';
+
+export const navItems: NavItems[] = [
+  { title: 'Inicio', link: '#inicio' },
+  { title: 'Habilidades', link: '#habilidades' },
+  { title: 'Proyectos', link: '#proyectos' },
+  { title: 'Contacto', link: '#contacto' }
+]
