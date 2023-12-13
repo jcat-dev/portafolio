e37312b1fe702d32c4efbb2de170e96ed8ebc6f5
unknown <jec.dev.22@gmail.com>
2023-12-12 23:52:47 -0300

    Renombrar y Agregar atributos en Profile.d.ts

--- src/Types/Profile.d.ts
+++ src/Types/Profile.d.ts
@@ -1,5 +1,10 @@
 export interface Profile {
-  title: string
-  description: string
+  stackTitle: string
+  fullName: string
   photoUrl: string
+  description: string
+}
+
+export interface ProfileWithId extends Profile {
+  _id: string
 }
