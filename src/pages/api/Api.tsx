cab90ef58b665d7cb61f955e0af6873d9cf41742
unknown <jec.dev.22@gmail.com>
2023-12-13 00:18:15 -0300

    Agregar Header.tsx en /pages/api/Api.tsx

--- src/pages/api/Api.tsx
+++ src/pages/api/Api.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from 'react-router-dom'
+import { navItems } from './navItems';
 import LayoutToast from '../../layout/LayoutToast';
+import Header from '../../component/header/Header';
 
 const Api = () => {
   return (
     <LayoutToast>
+      <Header 
+        navItems={navItems}
+        withNavLink={true}
+      />
       <Outlet />
     </LayoutToast>
   )
---
+++ src/pages/api/navItems.ts
@@ -0,0 +1,7 @@
+import { NavItems } from '../../Types/NavItems';
+
+export const navItems: NavItems[] = [
+  { title: 'Projects', link: '/api/projects' },
+  { title: 'Skills', link: '/api/skills' },
+  { title: 'Profile', link: '/api/profile' },
+]
