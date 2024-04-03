f5775f79c03fe9db19c4a532834ca11d0cea9deb
unknown <jec.dev.22@gmail.com>
2024-04-03 19:31:14 -0300

    Agregar props en component/particle/ParticlesBg.tsx
    Se elimina los estilos de ParticlesBg.tsx

--- src/component/particle/ParticlesBg.tsx
+++ src/component/particle/ParticlesBg.tsx
@@ -1,17 +1,18 @@
 import { useCallback } from 'react';
 import { loadFull } from 'tsparticles'
+import { getParticles, getParticlesWithLink } from './particles';
 import type { Engine } from 'tsparticles-engine'
 import Particles from 'react-tsparticles';
-import styles from './particlesBg.module.css'
-import { particleCircle, particleLinks } from './particles';
 
 interface Props {
   id: string
-  option: 'default' | 'amongUs'
-  className?: string
+  option: 'default' | 'particlesWithLink'
+  fullScreen: boolean
+  backgroundColor: string
+  containerClassName?: string
 }
 
-const ParticlesBg: React.FC<Props> = ({option, id, className}) => {
+const ParticlesBg: React.FC<Props> = ({option, id, fullScreen, backgroundColor, containerClassName}) => {
   const particlesInit = useCallback(async (engine: Engine) => {
     await loadFull(engine);
   }, []);
@@ -19,12 +20,12 @@ const ParticlesBg: React.FC<Props> = ({option, id, className}) => {
   return (
     <Particles    
       id={id}
-      className={`${styles['particles']} ${className ?? ''}`}
+      className={containerClassName ?? ''}
       init={particlesInit}
       options={
         option === 'default'
-          ? particleCircle
-          : particleLinks
+          ? getParticles(fullScreen, backgroundColor)
+          : getParticlesWithLink(fullScreen, backgroundColor)
       }
     />
   )
--- src/component/particle/particles.ts
+++ src/component/particle/particles.ts
@@ -1,144 +1,154 @@
 import type { RecursivePartial, IOptions} from 'tsparticles-engine'
 
-export const particleCircle: RecursivePartial<IOptions> = {
-  'detectRetina': true,
-  'fpsLimit': 120,
+export const getParticlesWithLink = (
+  fullScreen: boolean,
+  backgroundColor: string,
+): RecursivePartial<IOptions> => {
+  return {
+    'detectRetina': true,
+    'fpsLimit': 120,
 
-  'fullScreen': {
-    'enable': false,
-    'zIndex': -1
-  },
+    'fullScreen': {
+      'enable': fullScreen,
+      'zIndex': -1
+    },
 
-  'background': {
-    'color': {
-      'value': '#000000'
-    }
-  },
-
-  'particles': {
-    'color': {
-      'value': '#065186'
-    },
-    'links': {
-      'color': '#0c98eb',
-      'distance': 150,
-      'enable': true,
-      'opacity': 0.5,
-      'width': 2
-    },
-    'move': {
-      'enable': true,
-      'outModes': {
-        'default': 'bounce',
-        'top': 'bounce',
-        'bottom': 'bounce',
-        'left': 'bounce',
-        'right': 'bounce'
-      },
-      'random': true,
-      'speed': 1,
-      'straight': false
-    },
-    'number': {
-      'density': {
-        'enable': true,
-        'area': 100
-      },
-      'value': 15
-    },
-    'opacity': {
-      'value': 0.5
-    },
-    'shape': {
-      'type': 'circle' 
-    },
-    'size': {
-      'value': {
-        'min': 1,
-        'max': 5
+    'background': {
+      'color': {
+        'value': backgroundColor
       }
-    }
-  },
-
-  'interactivity': {
-    'events': {
-      'onClick': {
-        'enable': true,
-        'mode': 'push'
-      },
-      'onHover': {
-        'parallax': {
-          'enable': true,
-          'force': 10,
-          'smooth': 100
-        },
-        'enable': false,
-        'mode': 'repulse'
-      },
-      'resize': true
     },
-    'modes': {
-      'push': {
-        'quantity': 2
+
+    'particles': {
+      'color': {
+        'value': '#065186'
       },
-      'repulse': {
-        'distance': 150,
-        'duration': 0.5
+      'links': {
+        'color': '#0c98eb',
+        'distance': 100,
+        'enable': true,
+        'opacity': 0.5,
+        'width': 1
+      },
+      'move': {
+        'enable': true,
+        'outModes': {
+          'default': 'bounce',
+          'top': 'bounce',
+          'bottom': 'bounce',
+          'left': 'bounce',
+          'right': 'bounce'
+        },
+        'random': true,
+        'speed': 1,
+        'straight': false
+      },
+      'number': {
+        'density': {
+          'enable': true,
+          'area': 100
+        },
+        'value': 5
+      },
+      'opacity': {
+        'value': 0.5
+      },
+      'shape': {
+        'type': 'circle' 
+      },
+      'size': {
+        'value': {
+          'min': 1,
+          'max': 5
+        }
+      }
+    },
+
+    'interactivity': {
+      'events': {
+        'onClick': {
+          'enable': true,
+          'mode': 'push'
+        },
+        'onHover': {
+          'parallax': {
+            'enable': true,
+            'force': 10,
+            'smooth': 100
+          },
+          'enable': false,
+          'mode': 'repulse'
+        },
+        'resize': true
+      },
+      'modes': {
+        'push': {
+          'quantity': 2
+        },
+        'repulse': {
+          'distance': 150,
+          'duration': 0.5
+        }
       }
     }
   }
 }
 
-export const particleLinks: RecursivePartial<IOptions> = {
-  'detectRetina': true,
-  'fpsLimit': 120,
+export const getParticles = (
+  fullScreen: boolean,
+  backgroundColor: string,
+): RecursivePartial<IOptions>  => {
+  return {
+    'detectRetina': true,
+    'fpsLimit': 120,
 
-  'fullScreen': {
-    'enable': false,
-    'zIndex': -1
-  },
-
-  'background': {
-    'color': {
-      'value': '#000000'
-    }
-  },
-
-  'particles': {
-    'color': {
-      'value': '#065186'
+    'fullScreen': {
+      'enable': fullScreen,
+      'zIndex': -1
     },
-    'move': {
-      'direction': 'right',
-      'enable': true,
-      'outModes': {
-        'default': 'bounce',
-        'top': 'bounce',
-        'bottom': 'bounce',
-        'left': 'bounce',
-        'right': 'bounce'
+
+    'background': {
+      'color': {
+        'value': backgroundColor
+      }
+    },
+
+    'particles': {
+      'color': {
+        'value': '#065186'
       },
-      'random': false,
-      'speed': 1,
-      'straight': false
-    },
-    'number': {
-      'density': {
+      'move': {
+        'direction': 'right',
         'enable': true,
-        'area': 100
+        'outModes': {
+          'default': 'bounce',
+          'top': 'bounce',
+          'bottom': 'bounce',
+          'left': 'bounce',
+          'right': 'bounce'
+        },
+        'random': false,
+        'speed': 1,
+        'straight': false
       },
-      'value': 15
-    },
-    'opacity': {
-      'value': 0.5
-    },
-    'shape': {
-      'type': 'circle' 
-    },
-    'size': {
-      'value': {
-        'min': 1,
-        'max': 5
+      'number': {
+        'density': {
+          'enable': true,
+          'area': 100
+        },
+        'value': 15
+      },
+      'opacity': {
+        'value': 0.5
+      },
+      'shape': {
+        'type': 'circle' 
+      },
+      'size': {
+        'value': {
+          'min': 1,
+          'max': 5
+        }
       }
     }
   }
