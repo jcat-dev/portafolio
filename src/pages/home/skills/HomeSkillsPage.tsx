dc4275b913e84c1f4c3e42d2b75b681a0710be3b
unknown <jec.dev.22@gmail.com>
2024-04-08 17:53:55 -0300

    Renombrar y Actualizar pages/home/skills
    Se renombra Skills.tsx a HomeSkillsPage.tsx con su css.
    
    Se cambia el diseño de las tarjetas de stacks.
    
    Se elimina ParticleBg.

---
+++ src/pages/home/skills/HomeSkillsPage.tsx
@@ -0,0 +1,45 @@
+import { SkillWithId } from '../../../Types/Skill'
+import { useLoaderData } from 'react-router-dom'
+import styles from './homeSkillsPage.module.css'
+
+const HomeSkillsPage = () => {  
+  const skills = useLoaderData() as SkillWithId[]
+  
+  return (
+    <main className={styles['container']} >
+      <h1 className={styles['title']} >
+          Skills
+      </h1>
+
+      <ul className={styles['stacks']} > 
+        {
+          skills.map((value) => (
+            <li
+              key={value._id}
+              className={styles['stacks__item']}
+            >
+              <h2 className={styles['stacks__item-title']} >
+                {value.title}
+              </h2>
+
+              <ul className={styles['skills']}>
+                {
+                  value.skills.map((value, index) => (
+                    <li 
+                      key={index}
+                      className={styles['skills__item']}
+                    >
+                      {value}
+                    </li>
+                  ))
+                }
+              </ul>
+            </li>
+          ))
+        }    
+      </ul>   
+    </main>
+  )
+}
+
+export default HomeSkillsPage
