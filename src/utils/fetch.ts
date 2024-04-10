3e33ac61b844e49d2b3eb4e196c5d4314a6997a8
unknown <jec.dev.22@gmail.com>
2024-04-10 16:02:25 -0300

    Agregar Bearer Token en el headers de fetch.ts

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,12 +1,21 @@
 import { FetchMethod } from '../Types/FetchResponse'
 
 export const setFetch = (api: string, method: FetchMethod, body?: object) => {
+  const token = sessionStorage.getItem('token')
+
   const config: RequestInit = {
     method,
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     }
   } 
+
+  if (token) {
+    config.headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    }
+  }
 
   if (body) {
     config.body = JSON.stringify(body)
