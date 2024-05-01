8fd808029ff166e0f64a6c642c8420cba8abbdc9
unknown <jec.dev.22@gmail.com>
2024-04-30 23:13:47 -0300

    Agregar los archivos httpStatus.ts y toastMsg en /utils

---
+++ src/utils/httpStatus.ts
@@ -0,0 +1,3 @@
+export const OK_STATUS = 200
+export const CREATED_STATUS = 201
+export const NO_CONTENT_STATUS = 204
---
+++ src/utils/toastMsg.ts
@@ -0,0 +1,14 @@
+//validation
+export const VALIDATION_MSG = 'Campo Requerido'
+
+//profile
+export const PROFILE_LOADER_MSG = 'Error al cargar el perfil'
+
+//projects
+export const PROJECTS_LOADER_MSG = 'Error al cargar los proyectos'
+export const PROJECT_LOADER_MSG = 'Error al cargar el proyecto'
+export const STACK_PROJECTS_LOADER_MSG = 'Error al cargar los tipos de stacks'
+
+//skills
+export const SKILLS_LOADER_MSG = 'Error al cargar los skills'
+export const SKILL_LOADER_MSG = 'Error al cargar el skill'
