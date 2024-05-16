81ee52382276022b3356919c0b7fd284ac8bf2e9
unknown <jec.dev.22@gmail.com>
2024-05-16 17:23:53 -0300

    Cambiar el index de la vista /api/projects a /api/profile en /routers/apiRoutes.ts

--- src/routers/apiRoutes/apiRoutes.ts
+++ src/routers/apiRoutes/apiRoutes.ts
@@ -1,11 +1,11 @@
 import { RouteObject } from 'react-router-dom';
 import { apiProfileRoute } from './profile/apiProfileRoute';
-import Projects from '../../pages/api/projects/ApiProjectsPage';
+import { apiSkillsRoute } from './skills/apiSkillsRoute';
+import { apiProjectsRoute } from './projects/apiProjectsRoute';
+import { apiProfileLoader } from './profile/apiProfileLoader';
 import NotFound from '../../component/notFound/NotFound';
 import ApiPage from '../../pages/api/ApiPage';
-import { apiSkillsRoute } from './skills/apiSkillsRoute';
-import { apiProjectsLoader } from './projects/apiProjectsLoader';
-import { apiProjectsRoute } from './projects/apiProjectsRoute';
+import ApiProfilePage from '../../pages/api/profile/ApiProfilePage';
 
 export const apiRoutes: RouteObject = {
   path: '/api',
@@ -14,8 +14,8 @@ export const apiRoutes: RouteObject = {
   children: [
     {
       index: true,
-      Component: Projects,
-      loader: apiProjectsLoader,
+      Component: ApiProfilePage,
+      loader: apiProfileLoader,
     },    
     ...apiProjectsRoute,
     apiProfileRoute,
