7c57ea45f5ce167a43e9ba8a0b7f649406f07869
unknown <jec.dev.22@gmail.com>
2024-05-16 17:16:07 -0300

    Agregar los iconos para ‘lenguajes’ y ‘herramientas’ en /utils/Icons.ts

--- src/utils/Icons.ts
+++ src/utils/Icons.ts
@@ -1,6 +1,7 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import { faServer } from '@fortawesome/free-solid-svg-icons/faServer'
-import { faCode, faDatabase, faUserLock, faVial } from '@fortawesome/free-solid-svg-icons'
+import { faCode, faDatabase, faGears, faUserLock, faVial } from '@fortawesome/free-solid-svg-icons'
+import { faDev } from '@fortawesome/free-brands-svg-icons'
 
 interface Icons {
   title: string
@@ -27,5 +28,13 @@ export const icons: Icons[] = [
   {
     title: 'BACKEND',
     icon: faServer
+  },
+  {
+    title: 'LENGUAJES',
+    icon: faDev
+  },
+  {
+    title: 'HERRAMIENTAS',
+    icon: faGears
   }
 ]
