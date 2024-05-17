5a29bf8742859e8d4d081bbe947aab2ff535c095
unknown <jec.dev.22@gmail.com>
2024-05-16 23:26:50 -0300

    Agregar los enlaces de las redes en /pages/home/profile/HomeProfilePage.tsx

--- src/pages/home/profile/HomeProfilePage.tsx
+++ src/pages/home/profile/HomeProfilePage.tsx
@@ -1,5 +1,9 @@
 import { useLoaderData } from 'react-router-dom'
 import { ProfileWithId } from '../../../Types/Profile'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFilePdf } from '@fortawesome/free-regular-svg-icons'
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import Anchor from '../../../component/button/Anchor'
 import styles from './homeProfilePage.module.css'
 import ParticlesBg from '../../../component/particle/ParticlesBg'
 
@@ -17,28 +21,64 @@ const HomeProfilePage: React.FC = () => {
 
       <div className={styles['profile']} >
         <h1 className={styles['profile-title']} >
-          {
-            profile?.title
-          }
+          {profile?.title}
         </h1>
 
         <p className={styles['profile-fullname']}>
-          {
-            profile?.fullName
-          }
+          {profile?.fullName}
         </p>
         
         <h2 className={styles['profile-stack']} >
-          {
-            profile?.stackTitle
-          }
+          {profile?.stackTitle}
         </h2>
 
         <p className={styles['profile-description']} >
-          {
-            profile?.description
-          }
+          {profile?.description}
         </p>      
+
+        <div className={styles['profile-links']} >
+          <Anchor
+            href={import.meta.env.VITE_CV_URL}
+            target='_blank'
+            className={styles['cv-link']}
+          >
+            Descargar CV
+
+            <FontAwesomeIcon 
+              icon={faFilePdf} 
+              className={styles['cv-link-icon']}
+              size='xl'
+            />
+          </Anchor>    
+
+          <Anchor
+            href={import.meta.env.VITE_GITHUB_URL}
+            target='_blank'
+            className={styles['link']}
+          >
+            GitHub
+
+            <FontAwesomeIcon 
+              icon={faGithub}
+              className={styles['link-icon']}
+              size='xl'
+            />
+          </Anchor>  
+
+          <Anchor
+            href={import.meta.env.VITE_LINKEDIN_URL}
+            target='_blank'
+            className={styles['link']}
+          >
+            LinkedIn
+
+            <FontAwesomeIcon 
+              icon={faLinkedinIn}
+              className={styles['link-icon']}
+              size='xl'
+            />
+          </Anchor>     
+        </div>
       </div>      
     </main>
   )
